ff52317d2295a66e34b4870a009d9aa3bbeafd06
EmaCara98 <e.cara@itsrizzoli.it>
2023-06-26 09:08:26 +0200

    Second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,49 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/****************************** COMANDI PER EFFETTUARE I TEST ******************************/
+// Aprire: Windows Powershell
+// Digitare il comando: telnet 127.0.0.1 1234
+// Nella schermata nera digitare il comando: all
+// Poi digitare il comando: all_sorted
+// Poi digitare delle lettere a caso, deve mostrare "Comandi inesistente"
+
+
+/****************************** CODICE ******************************/
+public class App
 {
+    static final int portNumber = 1234;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Server started!");
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Socket clientSocket = null;
+
+        while(true)
+        {
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
+            WareHouse.getInstance().add(clientHandler);
+
+            Thread thread = new Thread(clientHandler);
+            thread.start();
+        }
     }
 }
---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private Socket clientSocket = null;
+    private InetAddress address;
+    private int port;
+    private PrintWriter out;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+
+        address = clientSocket.getInetAddress();
+        port = clientSocket.getPort();
+
+        System.out.println("Connected: " + address + "with port: " + port);
+    }
+
+    Boolean readLoop(BufferedReader in,  PrintWriter out ){
+
+        String s = "";
+
+        try {
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+
+                /********* COMANDI CLIENT *********/
+                switch(s)
+                {
+                    case "all":
+                        out.println(WareHouse.getInstance().all());
+                        break;
+                    case "all_sorted":
+                        out.println(WareHouse.getInstance().all_sorted());
+                        break;
+                    case "more_expensive":
+                        out.println(WareHouse.getInstance().more_expensive());
+                        break;
+                    default:
+                        out.println("Comando inesistente");
+                }
+            }
+
+            System.out.println("Disconnected: " + address + "with port: " + port);
+            WareHouse.getInstance().remove(this);
+            System.out.println("Now we have " + WareHouse.getInstance().nOfClients() + " connected client");
+
+            return true;
+
+        } catch (IOException e) {
+            System.out.println("Forcing disconnection for: " + address + "with port: " + port);
+        }
+
+        return false;
+    }
+
+    void handle()
+    {
+        out = null;
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            readLoop(in, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        handle();
+    }
+
+    /*
+    void write(String s)
+    {
+        out.println(s);
+        out.flush();
+    }
+    */
+
+}
